11b6d7ed633e96a3105d54c86423ab24884f9220
WFU <wfu@college.harvard.edu>
2019-01-30 14:37:45 -0500

    asdf

---
+++ comms/johnrobot.py
@@ -0,0 +1,22 @@
+from omni import OmniComms
+import numpy as np
+
+class JohnRobot(object):
+    """Class that controls John's old robot he made in high school. Essentially
+    the same as robocup except larger, slightly different wheels, and different
+    arduino used to control. Commands should still be in the same format"""
+
+    def __init__(self):
+        self.comms = OmniComms()
+
+    def move(self, forward, lateral, w):
+        """Move forward, laterally, and rotation. Should be given as a int
+        from 0 to 255, with 255 being the fastest."""
+        # Use -1 as first element to broadcast to all robots
+        robot_id = -1
+        f = int(np.clip(forward, 0, 255))
+        l = int(np.clip(lateral, 0, 255))
+        w = int(np.clip(w, 0, 255))
+        cmd = "{},{},{},{}".format(robot_id, f, l, w)
+        self.comms.send(cmd)
+

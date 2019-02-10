3723321601b32563dfba410674965f71a951d9dd
John <keszler@college.harvard.edu>
2019-02-10 16:23:27 -0500

    simplified visualization rendering code

--- vision/visualization.py
+++ vision/visualization.py
@@ -7,10 +7,16 @@ from data_providers import SSLVisionDataProvider
 # The size of our current field in ROBOCUP
 FIELD_W = 3200
 FIELD_H = 2400
-ROBOT_SIZE = 12
-BALL_SIZE = 5
 
+# rendering constants
+ROBOT_SIZE = 12
+ROBOT_COLOR = (0, 0, 0)
+
+BALL_SIZE = 5
 BALL_COLOR = (1, .5, 0)
+
+WAYPOINT_SIZE = 3
+WAYPOINT_COLOR = (0, 0, 0)
 
 # Scale for the display window, or else it gets too large...
 SCALE = 0.25
@@ -24,13 +30,9 @@ class GameState(object):
     def __init__(self):
         self.viewer = None
         self._ball = None # ball position
-        self._drawn_ball = None
         self._robots = dict()  # Dict of Robot ID (int) to x, y coord (numpy)
-        self._drawn_robot_txs = dict()  # Dict that deals with drawing robot transforms and rotations
         self._trajectories = dict()  # Dict of current trajectory plans for robot_id
-        self._drawn_trajectories = dict()
         self._waypoints = dict()  # Dict of current trajectory plans for robot_id
-        self._drawn_waypoints = dict()
         self.user_click = None
         self.user_click_field = None
 
@@ -58,6 +60,9 @@ class GameState(object):
             lateral_direction (right)]"""
         self._waypoints[robot_id] = waypoint
 
+    def scale_pos(self, tup):
+        return tuple([int(SCALE * n) for n in tup])
+
     def render(self):
         if self.viewer is None:
             # for single cam, viewer window boundaries should correspond to cam
@@ -65,7 +70,8 @@ class GameState(object):
 
             # Traps your mouse inside the screen
             # self.viewer.window.set_exclusive_mouse(True)
-            def on_mouse_press(x, y, button, modifiers):
+            # update user click position - coordinates flipped
+            def on_mouse_press(y, x, button, modifiers):
                 self.user_click = (x, y)
                 self.user_click_field = int(x / SCALE), int(y / SCALE)
 
@@ -75,45 +81,32 @@ class GameState(object):
 
         # Draw the ball
         if self._ball:
-            ball_screen_loc = self._ball * np.array([SCALE, SCALE])
+            ball_screen_loc = self.scale_pos(self._ball)
             t = rendering.Transform(translation=ball_screen_loc)
             self.viewer.draw_circle(BALL_SIZE, 20, color=BALL_COLOR).add_attr(t)
 
-        # Draw all of the robots as separate entities for robot_id, loc in self._robots: # If the robot hasn't been drawn yet, add it as a separate draw object. if robot_id not in self._drawn_robots:
+        # Draw all of the robots as separate entities:
         for robot_id, loc in self._robots.items():
-            if robot_id not in self._drawn_robot_txs:
-                # print("Adding a new robot into our drawn game state")
-                robot_points = [
-                    (ROBOT_SIZE, 0), (0, -ROBOT_SIZE), (-ROBOT_SIZE, 0),
-                    (0, ROBOT_SIZE), (ROBOT_SIZE, 0), (0, 0)]
-                drawn_robot = rendering.make_polyline(robot_points)
-                # drawn_robot.set_linewidth(5)
-                self._drawn_robot_txs[robot_id] = rendering.Transform()
-                drawn_robot.add_attr(self._drawn_robot_txs[robot_id])
-                
-                drawn_robot.set_color(0., 0., 0.)
-                self.viewer.add_geom(drawn_robot)
-            
-            # use the transform object to "move" the robot on-screen
-            loc *= np.array([SCALE, SCALE, 1.0])
-            scaled_x, scaled_y, w = loc[0], loc[1], loc[2]
-            self._drawn_robot_txs[robot_id].set_translation(scaled_x, scaled_y)
-            self._drawn_robot_txs[robot_id].set_rotation(np.pi / 2 - w)
+            # draw robot at screen-scaled location for this frame
+            robot_points = [
+                (ROBOT_SIZE, 0), (0, -ROBOT_SIZE), (-ROBOT_SIZE, 0),
+                (0, ROBOT_SIZE), (ROBOT_SIZE, 0), (0, 0)
+            ]
+            x, y, w = loc
+            robot_screen_loc = self.scale_pos((x, y))
+            robot_rotation = np.pi / 2 - w
+            t = rendering.Transform(translation=robot_screen_loc, rotation=robot_rotation)
+            self.viewer.draw_polyline(robot_points, color=ROBOT_COLOR).add_attr(t)
 
-            # if we have a trajectory object, we can draw that
+            # TODO: draw any trajectories for this robot
             if robot_id in self._trajectories:
                 pass
             
+            # draw any waypoints for this robot TODO: and line from robot to it?
             if robot_id in self._waypoints:
-                if robot_id not in self._drawn_waypoints:
-                    waypt = rendering.make_circle(5)
-                    self._drawn_waypoints[robot_id] = rendering.Transform() 
-                    waypt.add_attr(self._drawn_waypoints[robot_id])
-                    self.viewer.add_geom(waypt)
-                
-                loc = self._waypoints[robot_id]
-                x, y = int(loc[0] * SCALE), int(loc[1] * SCALE)
-                self._drawn_waypoints[robot_id].set_translation(x, y)
+                waypoint_screen_loc = self.scale_pos(self._waypoints[robot_id])
+                t = rendering.Transform(translation=waypoint_screen_loc)
+                self.viewer.draw_circle(WAYPOINT_SIZE, color=WAYPOINT_COLOR).add_attr(t)
     
         return self.viewer.render()
 

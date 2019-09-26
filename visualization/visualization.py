14bfbef9dd5e4250c00409447c796fed186debc5
grossmanlev <lgrossman@college.harvard.edu>
2019-09-26 17:12:58 -0400

    migrate visualizer to pygame

--- visualization/visualization.py
+++ visualization/visualization.py
@@ -1,50 +1,76 @@
-import pyglet
+import math
 import time
 import numpy as np
 import threading
-from gym.envs.classic_control import rendering
+import pygame
 
 # The size of the field (should match location data from ssl-vision)
 FIELD_W = 9000
 FIELD_H = 6000
 CENTER_CIRCLE_RADIUS = 495
+FIELD_LINE_WIDTH = 3
 
 # rendering constants
+FIELD_COLOR = (0, 255, 0)
+LINE_COLOR = (255, 255, 255)
+
 ROBOT_SIZE = 12
 ROBOT_COLOR = (0, 0, 0)
 
 BALL_SIZE = 5
-BALL_COLOR = (1, .5, 0)
+BALL_COLOR = (255, 125, 0)
 
 WAYPOINT_SIZE = 3
 WAYPOINT_COLOR = (0, 0, 0)
 
 # Scale for the display window, or else it gets too large... (pixels/mm)
 SCALE = 0.15
+# how much space to include outside the field
+WINDOW_BUFFER_PX = 20
+TOTAL_SCREEN_WIDTH = int(FIELD_W * SCALE) + WINDOW_BUFFER_PX * 2
+TOTAL_SCREEN_HEIGHT = int(FIELD_H * SCALE) + WINDOW_BUFFER_PX * 2
 
 class Visualizer(object):
     """Robocup homegrown visualization library that essentially does the same
     as the modules in OpenAI gym."""
     
     def __init__(self, gamestate):
+        self._viewer = None
+        self._clock = None
+        
         self.user_click = None
         self.user_click_field = None
 
         self._gamestate = gamestate
-        self._viewer = None
-        self._transform_ball = None
-        self._transform_waypoint = None
-        self._transform_robots = {} 
 
         self._updating = False
         self._visualization_thread = None
 
-    def scale_pos(self, tup):
+    # map ssl-vision field position to pixel x,y on viewer
+    def scale_pos(self, pos):
+        assert(len(pos) == 2 and type(pos) == tuple)
         # shift position so (0, 0) becomes the center of the field, as in ssl-vision
-        assert(len(tup) == 2)
-        tup = (tup[0] + FIELD_W / 2, tup[1] + FIELD_H / 2)
+        pos = (pos[0] + FIELD_W / 2, pos[1] + FIELD_H / 2)
         # scale for display
-        return tuple([int(SCALE * n) for n in tup])
+        pos = (int(pos[0] * SCALE), int(pos[1] * SCALE))
+        # account for buffer space outside of field
+        pos = (pos[0] + WINDOW_BUFFER_PX, pos[1] + WINDOW_BUFFER_PX)
+        # y becomes axis inverted in pygame (top left screen is 0,0)
+        pos = (pos[0], TOTAL_SCREEN_HEIGHT - pos[1])
+        return pos
+
+    # map screen pixels to field position
+    def unscale_pos(self, pos):
+        assert(len(pos) == 2 and type(pos) == tuple)
+        # revert y axis
+        pos = (pos[0], TOTAL_SCREEN_HEIGHT - pos[1])
+        # account for buffer space outside of field
+        pos = (pos[0] - WINDOW_BUFFER_PX, pos[1] - WINDOW_BUFFER_PX)
+        # unscale display
+        pos = (int(pos[0] / SCALE), int(pos[1] / SCALE))
+        # shift position so that center becomes (0, 0)
+        pos = (pos[0] - FIELD_W / 2, pos[1] - FIELD_H / 2)
+        return pos
 
     def start_visualizing(self):
         self._updating = True
@@ -52,11 +78,23 @@ class Visualizer(object):
         self._visualization_thread.start()
 
     def visualization_loop(self):
+        pygame.init()
+        self._viewer = pygame.display.set_mode((TOTAL_SCREEN_WIDTH, TOTAL_SCREEN_HEIGHT))
+        pygame.display.set_caption("Robocup Visualizer")
+        self._clock = pygame.time.Clock()
         while self._updating:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self._updating = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.user_click = pygame.mouse.get_pos()
+                    self.user_click_field = self.unscale_pos(self.user_click)                    
             start_time = time.time()
+            self._viewer.fill(FIELD_COLOR)
             self.render()
-            end_time = time.time()
-            print("Total Render Overhead: %s s" % str(end_time - start_time))
+            pygame.display.flip()
+            self._clock.tick(20)
+        pygame.quit()        
 
     def stop_visualizing(self):
         self._updating = False
@@ -64,93 +102,63 @@ class Visualizer(object):
         self._visualization_thread = None
 
     def render(self):
-        start_time = time.time()
-        if self._viewer is None:
-            # for single cam, viewer window boundaries should correspond to cam
-            self._viewer = rendering.Viewer(int(FIELD_W * SCALE), int(FIELD_H * SCALE))
+        assert(self._viewer is not None)
+        # draw field landmarks
+        pygame.draw.circle(
+            self._viewer,
+            LINE_COLOR,
+            self.scale_pos((0,0)),
+            int(CENTER_CIRCLE_RADIUS * SCALE),
+            FIELD_LINE_WIDTH
+        )
+        hw, hh = FIELD_W / 2, FIELD_H / 2
+        top_left = self.scale_pos((-hw, hh))
+        dims = (FIELD_W * SCALE, FIELD_H * SCALE)
+        boundary_lines_rect = [*top_left, *dims]
+        pygame.draw.rect(self._viewer, LINE_COLOR, boundary_lines_rect, FIELD_LINE_WIDTH)
+        pygame.draw.line(
+            self._viewer,
+            LINE_COLOR,
+            self.scale_pos((0, hh)),
+            self.scale_pos((0, -hh)),
+            FIELD_LINE_WIDTH
+        )
 
-            # draw field landmarks
-            center_circle = rendering.make_circle(CENTER_CIRCLE_RADIUS * SCALE)
-            circle_transform = rendering.Transform()
-            circle_transform.set_translation(*self.scale_pos((0, 0)))
-            center_circle.add_attr(circle_transform)
-            center_circle.set_color(.9, .9, .9)
-            self._viewer.add_geom(center_circle)
-            hw, hh = FIELD_W / 2, FIELD_H / 2
-            corner_points = [(-hw, -hh), (-hw, hh),
-                             (0, hh), (0, -hh), (0, hh),
-                             (hw, hh), (hw, -hh), (-hw, -hh)]
-            scaled_corner_points = [self.scale_pos(p) for p in corner_points]            
-            boundary_lines = rendering.make_polyline(scaled_corner_points)
-            boundary_lines_transform = rendering.Transform()
-            boundary_lines_transform.set_translation(*self.scale_pos((-hw, -hh)))
-            boundary_lines.add_attr(boundary_lines_transform)
-            boundary_lines.set_linewidth(2)
-            boundary_lines.set_color(0, 0, 0)
-            self._viewer.add_geom(boundary_lines)
+        # Draw ball
+        pygame.draw.circle(
+            self._viewer,
+            BALL_COLOR,
+            self.scale_pos(self._gamestate.ball),
+            BALL_SIZE
+        )
 
-            # Traps your mouse inside the screen
-            # self.viewer.window.set_exclusive_mouse(True)
-            # update user click position - coordinates flipped
-            def on_mouse_press(x, y, button, modifiers):
-                self.user_click = (x, y)
-                self.user_click_field = int(x / SCALE), int(y / SCALE)
-
-            self._viewer.window.on_mouse_press = on_mouse_press
-
-            # Ball object and transform.
-            drawn_ball = rendering.make_circle(BALL_SIZE)
-            self._transform_ball = rendering.Transform()
-            drawn_ball.add_attr(self._transform_ball)
-            drawn_ball.set_color(*BALL_COLOR)
-            self._viewer.add_geom(drawn_ball)
-
-            # Waypoint
-            drawn_waypoint = rendering.make_circle(WAYPOINT_SIZE)
-            self._transform_waypoint = rendering.Transform()
-            drawn_waypoint.add_attr(self._transform_waypoint)
-            drawn_waypoint.set_color(*WAYPOINT_COLOR)
-            self._viewer.add_geom(drawn_waypoint)
-        # Draw the ball
-        if self._gamestate.ball:
-            # print(self._transform_ball)
-            self._transform_ball.set_translation(*self.scale_pos(self._gamestate.ball))
-        # Draw all of the robots as separate entities:
-        # print(self._gamestate.robots)
-        for robot_id, loc in self._gamestate.robots.items():
-            # draw robot at screen-scaled location for this frame if robot has
-            # not yet been drawn so far, otherwise just set the location
-            # of the rendered object
-            if robot_id not in self._transform_robots:
-                self._transform_robots[robot_id] = rendering.Transform()
-                robot_points = [(ROBOT_SIZE, 0), (0, -ROBOT_SIZE), (-ROBOT_SIZE, 0),
-                                (0, ROBOT_SIZE), (ROBOT_SIZE, 0), (0, 0)]
-                drawn_robot = rendering.make_polyline(robot_points)
-                # drawn_robot.set_linewidth(5)
-                self._transform_robots[robot_id] = rendering.Transform()
-                drawn_robot.add_attr(self._transform_robots[robot_id])
-
-                drawn_robot.set_color(*ROBOT_COLOR)
-                self._viewer.add_geom(drawn_robot)           
+        # draw user click location
+        if self.user_click:
+            pygame.draw.circle(self._viewer, (0, 0, 0), self.user_click, 5)
             
-            # Place the rendered robot in the correct location
-            x, y, w = loc
-            robot_screen_loc = self.scale_pos((x, y))
-            robot_rotation = w
-            self._transform_robots[robot_id].set_translation(*robot_screen_loc)
-            self._transform_robots[robot_id].set_rotation(robot_rotation)
+        # Draw all the robots
+        for robot_id, loc in self._gamestate.robots.items():
+            (x, y, w) = loc
+            pygame.draw.circle(
+                self._viewer,
+                ROBOT_COLOR,
+                self.scale_pos((x, y)),
+                ROBOT_SIZE
+            )
+            # indicate direction of robot
+            arrow_scale = ROBOT_SIZE * 6
+            pygame.draw.line(
+                self._viewer,
+                (255, 0, 0),
+                self.scale_pos((x, y)),
+                self.scale_pos((x + math.cos(w) * arrow_scale, y + math.sin(w) * arrow_scale)),
+                1
+            )
 
             # TODO: draw any trajectories for this robot
             if robot_id in self._gamestate.trajectories:
                 pass
             
-            # draw any waypoints for this robot TODO: and line from robot to it?
-            if robot_id in self._gamestate.waypoints:
-                waypoint_screen_loc = self.scale_pos(self._gamestate.waypoints[robot_id])
-                self._transform_waypoint.set_translation(*waypoint_screen_loc)
-        self._viewer.render()
-        return True
-
     def close(self):
         if self._viewer is not None:
             self._viewer.close()

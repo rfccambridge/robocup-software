8170ac5d781fa60c6a222958c10d9211efd5521f
grossmanlev <lgrossman@college.harvard.edu>
2019-11-04 14:15:13 -0500

    comms refactor in progress

--- comms/comms.py
+++ comms/comms.py
@@ -2,6 +2,7 @@ import threading
 import time
 import math
 import numpy as np
+from collections import deque
 from .robot import Robot
 from .omni import OmniComms
 
@@ -18,6 +19,12 @@ ROTATION_SPEED_SCALE = 0
 
 # how long a command can be run without being updated
 COMMAND_DURATION = .2
+# limiting throughput of messages sent to robot
+COMMAND_THROTTLE_INTERVAL = .15
+COMMAND_THROTTLE_CAPACITY = 3
+CMD_MOVE = 0
+CMD_DRIBBLE = 1
+CMD_KILL = 2
 
 
 class Comms(object):
@@ -28,29 +35,28 @@ class Comms(object):
         self._gamestate = gamestate
         assert(team in ['blue', 'yellow'])
         self._team = team
-        self._robots = dict() # dict from id to robot (comms class)
+
+        self._comms = OmniComms()
+
         # store previously sent commands to reduce redundant commands
         self._robot_dribblers = dict()  # Dict of previously sent dribbler speeds for robot_id
         self._robot_chargings = dict()  # Dict of previously sent kicker chargings for robot_id
+        self._robot_last_command_times = dict()
         
         self._is_sending = False
         self._sending_thread = None
-        self._last_sent_time = None
+        self._last_send_loop_time = None
 
         self._is_receiving = False
         self._receiving_thread = None
         self._messages_received = []
-        self._comms = OmniComms()
+        self._last_receive_loop_time = None
+
 
     def die(self):
         self._comms.close()
-        for robot in self._robots:
-            robot.die()
 
     def start_sending(self):
-        for robot_id in self._gamestate.get_robot_ids(self._team):
-            self._robots[robot_id] = Robot()
-        
         self._is_sending = True
         self._sending_thread = threading.Thread(target=self.sending_loop)
         # set to daemon mode so it will be easily killed
@@ -69,17 +75,11 @@ class Comms(object):
             for robot_id in self._gamestate.get_robot_ids(self._team):
                 robot_commands = self._gamestate.get_robot_commands(self._team, robot_id)
                 waypoints = robot_commands.waypoints
-                if robot_id not in self._robots:
-                    print("Commanding a Robot that was not connected when initializing")
-                    print("(not seen by vision) - trying to connect now...")
-                    self._robots[robot_id] = Robot()
-                    
-                robot = self._robots[robot_id]
 
                 pos = self._gamestate.get_robot_position(self._team, robot_id)
                 # stop the robot if we've lost track of it
                 if self._gamestate.is_robot_lost(self._team, robot_id):
-                    robot.move(0, 0, 0, COMMAND_DURATION)
+                    self.move_command(robot_id, 0, 0, 0, COMMAND_DURATION)
                     continue
                 og_x, og_y, og_w = pos
                 # if close enough to first waypoint, delete and move to next one
@@ -102,24 +102,25 @@ class Comms(object):
                     # move with speed proportional to delta
                     linear_speed = self.magnitude(delta) * SPEED_SCALE
                     linear_speed = min(min_speed + linear_speed, max_speed)
-                    robot.move(linear_speed * norm_x,
-                               linear_speed * norm_y,
-                               norm_w * ROTATION_SPEED_SCALE,
-                               COMMAND_DURATION)
+                    self.move_command(
+                        robot_id,
+                        linear_speed * norm_x,
+                        linear_speed * norm_y,
+                        norm_w * ROTATION_SPEED_SCALE,
+                        COMMAND_DURATION)
 
                 # send dribbler commands
                 dribbler_speed = robot_commands.dribbler_speed
-                if dribbler_speed != self._robot_dribblers.get(robot_id, None):
-                    self._robot_dribblers[robot_id] = dribbler_speed
-                    robot.dribble(dribbler_speed)
+                self.dribble_command(robot_id, dribbler_speed)                    
 
                 # TODO: send other commands for dribbler and kicking
-            if self._last_sent_time is not None:
-                delta = time.time() - self._last_sent_time
+            if self._last_send_loop_time is not None:
+                delta = time.time() - self._last_send_loop_time
                 if delta > .3:
                     print("Comms sending loop unexpectedly large delay: " + str(delta))
-            self._last_sent_time = time.time()
+            self._last_send_loop_time = time.time()
             # yield to other threads - run this loop at most 20 times per second
+            # Warning - looping too fast might overload throttle, so some commands never sent
             time.sleep(.05)
 
     def receiving_loop(self):
@@ -127,11 +128,11 @@ class Comms(object):
             # TODO: save messages for log
             print(self._comms.read())
             # TODO: update relevant data into gamestate
-            if self._last_sent_time is not None:
-                delta = time.time() - self._last_sent_time
+            if self._last_receive_loop_time is not None:
+                delta = time.time() - self._last_receive_loop_time
                 if delta > .3:
                     print("Comms receiving loop unexpectedly large delay: " + str(delta))
-            self._last_sent_time = time.time()
+            self._last_receive_loop_time = time.time()
             # yield to other threads - run this loop at most 20 times per second
             time.sleep(.05)
 
@@ -144,19 +145,16 @@ class Comms(object):
         DISTANCE_THRESHOLD = 10
         return (dx + dy) ** .5 < DISTANCE_THRESHOLD
 
-    def stop_sending(self):
+    def stop_sending_and_receiving(self):
         if self._is_sending:
             self._is_sending = False
             self._sending_thread.join()
-            self.die()
             self._sending_thread = None
-
-    def stop_receiving(self):
         if self._is_receiving:
             self._is_receiving = False
             self._receiving_thread.join()
-            self.die()
             self._receiving_thread = None
+        self.die()
 
     # coordinate math helper functions
     def normalize(self, w_robot, vector):
@@ -167,7 +165,7 @@ class Comms(object):
             return vector
         x, y = vector
         w_rot = w_robot - np.arctan2(y, x)
-        return (np.sin(w_rot), np.cos(w_rot))
+        return (np.sin(w_rot), np.cos(w_rot))    
 
     def magnitude(self, v):
         x, y = v
@@ -184,3 +182,47 @@ class Comms(object):
         if angle < -math.pi:
             angle += 2 * math.pi
         return angle
+
+    # COMMAND FUNCTIONS - not guaranteed to send (can repeatedly call in a loop)
+    # sends command if throttle conditions are met, returns whether command was sent
+    def try_send_command(self, robot_id, command_string):
+        if robot_id not in self._robot_last_command_times:
+            self._robot_last_command_times[robot_id] = deque([], COMMAND_THROTTLE_CAPACITY)
+        earliest_time = self._robot_last_command_times[robot_id].
+        can_send = time.time() - self._robot_last_command_times[robot_id] > COMMAND_DELAY
+        if can_send:
+            self.comms.send(command_string)
+            self._robot_last_command_times[robot_id] = time.time()
+        return can_send
+
+    def kill_command(self):
+        # TODO: where is this used? ever for single robot?
+        # Use robot_id = -1 to broadcast to all robots
+        robot_id = -1
+        cmd = "{},{}".format(robot_id, CMD_KILL)
+        # for now, kill bypasses throughput restrictions
+        self.comms.send(cmd)
+
+    def move_command(self, robot_id, x, y, w, ttl=0.5):
+        """Move x, y, and rotation velocity - x, y are mm/s, 
+           and y is radians/s of the robot"""
+        robot_id = -1 # TODO delete
+        time_ms = int(ttl * 1000.0)
+        cmd = "{},{},{},{},{},{}".format(robot_id, CMD_MOVE, x, y, w, time_ms)
+        self.try_send_command(cmd)
+
+    def dribble_command(self, robot_id, power):
+        # only send command if its a change in speed
+        if dribbler_speed != self._robot_dribblers.get(robot_id, None):                
+            robot_id = -1 # TODO delete
+            cmd = "{},{},{}".format(robot_id, CMD_DRIBBLE, power)
+            if self.try_send_command(cmd):
+                self._robot_dribblers[robot_id] = dribbler_speed
+
+    def kick_command(self, robot_id):
+        raise NotImplementedError
+
+    def charge_command(self, robot_id):
+        """IMMA CHARGIN MY KICKER!!!!!"""
+        raise NotImplementedError
+

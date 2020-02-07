6a61513e311f2665cc18c834a461bfd848f0bdd0
Robocup Computer <harvardrobocup@gmail.com>
2020-02-07 15:26:06 -0500

    Refbox client fixed courtsey of Dr R. Lin

--- refbox/refbox.py
+++ refbox/refbox.py
@@ -1,8 +1,9 @@
-from socket import socket, AF_INET, SOCK_DGRAM, IPPROTO_IP, IP_ADD_MEMBERSHIP, inet_aton, INADDR_ANY, SOL_SOCKET, SO_REUSEADDR
+import socket
 from struct import pack
 import binascii
 from ipaddress import ip_address
 
+# from referee_pb2 import SSL_Referee_Game_Event
 from referee_pb2 import SSL_Referee
 
 class RefboxClient:
@@ -11,29 +12,31 @@ class RefboxClient:
         self.ip = ip
         self.port = port
 
+
     def connect(self):
         """Binds the client with ip and port and configure to UDP multicast."""
-
-        connection_info = (self.ip, self.port)
 
         if not isinstance(self.ip, str):
             raise ValueError('IP type should be string type')
         if not isinstance(self.port, int):
             raise ValueError('Port type should be int type')
         
-        self.sock = socket(AF_INET, SOCK_DGRAM)
-        self.sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-        # For some reason with Ubuntu on Windows (WSL) you need to first bind to 0.0.0.0, before changing the address
-        self.sock.bind(('0.0.0.0', connection_info[1]))
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, pack("=4sl", socket.inet_aton(self.ip), socket.INADDR_ANY))
+        self.sock.bind((self.ip, self.port))
 
-        if ip_address(connection_info[0]).is_multicast:
-            self.sock.setsockopt(IPPROTO_IP, IP_ADD_MEMBERSHIP, pack('=4sl', inet_aton(connection_info[0]), INADDR_ANY))
-        
+       
+    """
+        host = socket.gethostbyname(socket.gethostname())
+        self.sock.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_IF, socket.inet_aton(host))
+        self.sock.setsockopt(socket.SOL_IP, socket.IP_ADD_MEMBERSHIP, 
+                socket.inet_aton(self.ip) + socket.inet_aton(host))
+ """
+
     def receive(self):
         """Receive package and decode."""
-        packet = SSL_Referee()
 
-        data = self.sock.recv(1024)
-
-        packet.ParseFromString(data)
-        print(packet)
+        data, _ = self.sock.recvfrom(1024)
+        decoded_data = SSL_Referee.FromString(data)
+        print(decoded_data)
+        return decoded_data

43cc97f1578090ea0520c0609c7835395b026eab
Kendall Zhu <kendallzhu66@gmail.com>
2020-05-14 21:10:32 -0400

    add one test

--- gamestate/tests/test_field.py
+++ gamestate/tests/test_field.py
@@ -1,7 +1,11 @@
 # pylint: disable=import-error
+from ..gamestate import GameState
 from ..gamestate_field import Field
 
 
-def test_funcs():
-    field = Field()
-    assert True
+def test_defense_area():
+    gs = GameState()
+    team = 'blue'
+    # middle of goal should be in defense area
+    goalposts = gs.get_defense_goal(team)
+    assert gs.is_in_defense_area(goalposts[0], team)

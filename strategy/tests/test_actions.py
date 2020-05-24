9f4dda2725b40ef6bac160704ae55aeafa0aec83
eda214 <edward_athaide@college.harvard.edu>
2020-05-24 15:32:38 -0500

    Path find tests written

--- strategy/tests/test_actions.py
+++ strategy/tests/test_actions.py
@@ -6,14 +6,29 @@ from simulator.simulator import Simulator
 team = "blue"
 strategy_name = ""
 
-def test_path_find():
-    simulator = Simulator("full_teams")
+def test_path_find_clear_field():
+    """ Tests path_find when there is a clear path to the goal position.
+    Passes if the robot is successfully directed to the goal.
+    """
+    simulator = Simulator("clear_field_test")
     simulator.pre_run()
     gs = simulator.gs
     strategy = Strategy(team, strategy_name)
     strategy.gs = gs
-    print(gs.get_robot_position(team, 1))
     strategy.path_find(1, [0, 0, 0])
-    print(strategy.gs.get_robot_commands(team, 1))
-    assert(False)
+    goal_pos = strategy.get_goal_pos(1)
+    assert((goal_pos == [0, 0, 0]).all())
+
+def test_path_find_surrounded():
+    """ Tests path_find when the robot is surrounded by opponents and cannot find a path.
+    Passes if the robot is not given any waypoints to try to reach this destination.
+    """
+    simulator = Simulator("surrounded_by_opponents_test")
+    simulator.pre_run()
+    gs = simulator.gs
+    strategy = Strategy(team, strategy_name)
+    strategy.gs = gs
+    strategy.path_find(1, [0, 0, 0])
+    goal_pos = strategy.get_goal_pos(1)
+    assert(goal_pos is None)
     

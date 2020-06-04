cf1aeb0ec5fb56ba54cc1a9ffff17e60e45316c5
eda214 <edward_athaide@college.harvard.edu>
2020-06-04 12:51:56 -0500

    Style fixes for actions tests

--- strategy/tests/test_actions.py
+++ strategy/tests/test_actions.py
@@ -8,6 +8,7 @@ from simulator.simulator import Simulator
 team = "blue"
 strategy_name = ""
 
+
 def test_pivot_with_ball():
     """ Tests pivot_with_ball in simple case of empty field and 90 degree turn.
     Passes if the robot begins to turn in the shorter path to the goal direction
@@ -23,8 +24,9 @@ def test_pivot_with_ball():
     ball_pos = gs.get_ball_position()
     strategy.pivot_with_ball(1, np.array([0, 1000]))
     goal_pos = strategy.get_goal_pos(1)
-    assert(goal_pos[2] > start_pos[2])
-    assert(np.linalg.norm(goal_pos[:2] - ball_pos) < gs.ROBOT_RADIUS * CLOSE_ENOUGH_FACTOR)
+    assert goal_pos[2] > start_pos[2]
+    assert np.linalg.norm(goal_pos[:2] - ball_pos) < gs.ROBOT_RADIUS * CLOSE_ENOUGH_FACTOR
+
 
 def test_charge_up_to():
     """ Tests charge_up_to in the trivial case of zero kick speed.
@@ -36,7 +38,8 @@ def test_charge_up_to():
     strategy = Strategy(team, strategy_name)
     strategy.gs = gs
     charge = strategy.charge_up_to(1, 0.0)
-    assert(charge)
+    assert charge
+
 
 def test_kick_ball():
     """Tests kick_ball to make sure it sets is_kicking to true"""
@@ -47,7 +50,8 @@ def test_kick_ball():
     strategy.gs = gs
     strategy.kick_ball(1)
     commands = gs.get_robot_commands(team, 1)
-    assert(commands.is_kicking)
+    assert commands.is_kicking
+
 
 def test_set_dribbler():
     """ Tests set_dribbler after setting to True and to False.
@@ -60,10 +64,11 @@ def test_set_dribbler():
     strategy.gs = gs
     strategy.set_dribbler(1, True)
     commands = gs.get_robot_commands(team, 1)
-    assert(commands.is_dribbling)
+    assert commands.is_dribbling
     strategy.set_dribbler(1, False)
     commands = gs.get_robot_commands(team, 1)
-    assert(not commands.is_dribbling)
+    assert not commands.is_dribbling
+
 
 def test_stop():
     """ Tests stop function.
@@ -77,8 +82,9 @@ def test_stop():
     strategy.path_find(1, [0, 0, 0])
     strategy.stop(1)
     commands = gs.get_robot_commands(team, 1)
-    assert(not commands.waypoints)
-    assert(commands._x == 0 and commands._y == 0 and commands._w == 0)
+    assert not commands.waypoints
+    assert commands._x == 0 and commands._y == 0 and commands._w == 0
+
 
 def test_move_straight():
     """ Tests move_straight on a clear field.
@@ -92,8 +98,9 @@ def test_move_straight():
     strategy.gs = gs
     strategy.path_find(1, goal_pos)
     waypoints = gs.get_robot_commands(team, 1).waypoints
-    assert(len(waypoints) == 1 and (waypoints[0] == goal_pos).all())
+    assert len(waypoints) == 1 and (waypoints[0] == goal_pos).all()
     
+
 def test_path_find_clear_field():
     """ Tests path_find when there is a clear path to the goal position.
     Passes if the robot is successfully directed to the goal.
@@ -105,7 +112,8 @@ def test_path_find_clear_field():
     strategy.gs = gs
     strategy.path_find(1, [0, 0, 0])
     goal_pos = strategy.get_goal_pos(1)
-    assert((goal_pos == [0, 0, 0]).all())
+    assert (goal_pos == [0, 0, 0]).all()
+
 
 def test_path_find_surrounded():
     """ Tests path_find when the robot is surrounded by opponents and cannot find a path.
@@ -118,5 +126,5 @@ def test_path_find_surrounded():
     strategy.gs = gs
     strategy.path_find(1, [0, 0, 0])
     goal_pos = strategy.get_goal_pos(1)
-    assert(goal_pos is None)
+    assert goal_pos is None
     
